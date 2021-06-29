e894703a2aa7e3d0f651f0256957e7ebbd129a2c
Nayeer Bin Mahiuddin <46400232+NayeerM@users.noreply.github.com>
2021-06-29 15:52:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from sendEmail import EMailClient
 from pymongo import MongoClient
 
 app = Flask(__name__)  # initialising the flask app with the name 'app'
-#os.environ["PORT"]="5000"
 
 # geting and sending response to dialogflow
 @app.route('/webhook', methods=['POST'])
@@ -38,7 +37,7 @@ def processRequest(req):
     cust_name = parameters.get("cust_name")
     cust_contact = parameters.get("cust_contact")
     cust_email = parameters.get("cust_email")
-    db = configureDataBase()
+    #db = configureDataBase()
 
     if intent == 'covid_searchcountry':
         cust_country = parameters.get("geo-country")
@@ -55,8 +54,8 @@ def processRequest(req):
             deaths_data.get('new')) + \
                           "\n" + " Total Test Done : " + str(deaths_data.get('total')) + "\n\n*******END********* \n "
         print(webhookresponse)
-        log.saveConversations(sessionID, cust_country, webhookresponse, intent, db)
-        log.saveCases( "country", fulfillmentText, db)
+        #log.saveConversations(sessionID, cust_country, webhookresponse, intent, db)
+        #log.saveCases( "country", fulfillmentText, db)
 
         return {
 
@@ -82,13 +81,13 @@ def processRequest(req):
         }
     elif intent == "Welcome" or intent == "continue_conversation" or intent == "not_send_email" or intent == "endConversation" or intent == "Fallback" or intent == "covid_faq" or intent == "select_country_option":
         fulfillmentText = result.get("fulfillmentText")
-        log.saveConversations(sessionID, query_text, fulfillmentText, intent, db)
+        #log.saveConversations(sessionID, query_text, fulfillmentText, intent, db)
     elif intent == "send_report_to_email":
         fulfillmentText = result.get("fulfillmentText")
-        log.saveConversations(sessionID, "Sure send email", fulfillmentText, intent, db)
-        val = log.getcasesForEmail("country", "", db)
-        print("===>",val)
-        prepareEmail([cust_name, cust_contact, cust_email,val])
+        #log.saveConversations(sessionID, "Sure send email", fulfillmentText, intent, db)
+        #val = log.getcasesForEmail("country", "", db)
+        #print("===>",val)
+        #prepareEmail([cust_name, cust_contact, cust_email,val])
     elif intent == "totalnumber_cases":
         fulfillmentText = makeAPIRequest("world")
 
@@ -102,7 +101,7 @@ def processRequest(req):
                           "\n" + " Last updated : " + str(
             fulfillmentText.get('last_update')) + "\n\n*******END********* \n "
         print(webhookresponse)
-        log.saveConversations(sessionID, "Cases worldwide", webhookresponse, intent, db)
+        #log.saveConversations(sessionID, "Cases worldwide", webhookresponse, intent, db)
         #log.saveCases("world", fulfillmentText, db)
 
         return {
